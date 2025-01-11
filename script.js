3de499f2db175b5a848fab1fe80b4de48539f5e8
Issafulldev <aissa.azgr@protonmail.com>
2025-01-12 02:47:15 +0700

    JS check and score ok

--- script.js
+++ script.js
@@ -1,3 +1,27 @@
 'use strict';
 
-document.querySelector('.message')
+const secretNumber = Math.trunc(Math.random() * 20) + 1;
+let score = 20;
+
+document.querySelector('.number').textContent = secretNumber;
+
+document.querySelector('.check').addEventListener('click', function () {
+  const guess = Number(document.querySelector('.guess').value)
+  console.log(guess, typeof guess);
+
+  if (!guess) {
+    document.querySelector('.message').textContent = '⛔️ No secretNumber!';
+  } else if (guess === secretNumber) {
+    document.querySelector('.message').textContent = '🎉 Correct Number!';
+    score++;
+    document.querySelector('.score').textContent = score;
+  } else if (guess > secretNumber) {
+    document.querySelector('.message').textContent = '📈 Too High!';
+    score--;
+    document.querySelector('.score').textContent = score;
+  } else if (guess < secretNumber) {
+    document.querySelector('.message').textContent = '📉 Too Low!';
+    score--;
+    document.querySelector('.score').textContent = score;
+  }
+})
